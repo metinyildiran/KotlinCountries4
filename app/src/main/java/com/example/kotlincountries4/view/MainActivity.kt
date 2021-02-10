bfec2849e6376b4b02255df76c8a2c5be5fa256e
Metin Yıldıran <metin.1464@gmail.com>
2021-02-10 21:49:50 +0300

    20. ders bitti

--- app/src/main/java/com/example/kotlincountries4/view/MainActivity.kt
+++ app/src/main/java/com/example/kotlincountries4/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kotlincountries4
+package com.example.kotlincountries4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlincountries4.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
